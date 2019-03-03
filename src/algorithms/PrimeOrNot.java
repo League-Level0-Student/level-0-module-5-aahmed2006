1aed84da1b0ec039cb8879732f8e3f9dcea68ade
e-luu <league@WTS22.attlocal.net>
2019-03-02 17:27:09 -0800

    Prime or Not

---
+++ src/algorithms/PrimeOrNot.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+	public static void main(String[] args) {
+		String number = JOptionPane.showInputDialog("What is your number?");
+
+	}
+}
