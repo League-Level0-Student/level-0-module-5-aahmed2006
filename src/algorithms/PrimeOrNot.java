f3b0d95d80fdc2bb2c2718ceee198b28f9e9342a
aahmed2006 <league@WTS-IM-6.attlocal.net>
2019-03-09 17:25:57 -0800

    Prime or not, circle ring, for loop gauntlet, guessing pi, goofy names

--- src/algorithms/PrimeOrNot.java
+++ src/algorithms/PrimeOrNot.java
@@ -1,10 +1,20 @@
 package algorithms;
 
 import javax.swing.JOptionPane;
+import javax.xml.bind.helpers.ParseConversionEventImpl;
 
 public class PrimeOrNot {
 	public static void main(String[] args) {
 		String number = JOptionPane.showInputDialog("What is your number?");
-
+		int num = Integer.parseInt(number);
+		for(int i = 2; i<num; i++) {
+		 if(num%i==0) {
+			 JOptionPane.showMessageDialog(null, "Your number is not prime.");
+			 break;
+		 } else {
+			 JOptionPane.showMessageDialog(null, "Your number is prime.");
+			 break;
+		 }
+		}
 	}
 }
---
+++ src/nested_loops/ForLoopGauntlet.java
@@ -0,0 +1,20 @@
+package nested_loops;
+
+public class ForLoopGauntlet {
+public static void main(String[] args) {
+	for(int i = 0; i < 101; i++) {
+		System.out.println(i);
+	}
+	for(int j = 100; j > -1; j--) {
+		System.out.println(j);
+	}
+	for(int a = 0; a > 100; a++) {
+		
+		
+		}
+	}
+	
+	
+}
+
+
