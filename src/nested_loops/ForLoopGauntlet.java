8fa6e679c669ea49332d831001a66dff2d12ac0c
jbear18 <league@WTS22.attlocal.net>
2019-03-23 17:29:41 -0700

    For Loop Gauntlet

--- src/nested_loops/ForLoopGauntlet.java
+++ src/nested_loops/ForLoopGauntlet.java
@@ -1,20 +1,73 @@
 package nested_loops;
 
 public class ForLoopGauntlet {
-public static void main(String[] args) {
-	for(int i = 0; i < 101; i++) {
-		System.out.println(i);
-	}
-	for(int j = 100; j > -1; j--) {
-		System.out.println(j);
-	}
-	for(int a = 0; a > 100; a++) {
-		
-		
+	public static void main(String[] args) {
+		// for (int i = 0; i < 101; i++) {
+		// System.out.println(i);
+		// }
+
+		// for (int j = 100; j > -1; j--) {
+		// System.out.println(j);
+		// }
+
+		// for (int a = 2; a < 101; a = a + 2) {
+		// System.out.println(a);
+		// }
+
+		// for (int b = 1; b < 100; b = b + 2) {
+		// System.out.println(b);
+		// }
+
+		// for (int c = 0; c < 501; c++) {
+		// if (c % 2 == 0) {
+		// System.out.println(c + " is even");
+		// } else if (c % 2 == 1) {
+		// System.out.println(c + " is odd");
+		// }
+		// }
+
+		// for (int d = 0; d < 701; d++) {
+		// if (d % 7 == 0) {
+		// System.out.println(d);
+		// }
+		// }
+
+		// for (int e = 2006; e < 2020; e++) {
+		// System.out.println(e + " I was ");
+		// }
+
+		// for (int i = 0; i < 3; i++) {
+		//
+		// for (int ii = 0; ii < 3; ii++) {
+		// System.out.print(i);
+		// System.out.println(ii);
+		//
+		// }
+		//
+		// }
+
+		// int a = 1;
+		// int b = 2;
+		// int c = 3;
+		// for (int i = 0; i < 3; i++) {
+		// System.out.println(a + " " + b + " " + c);
+		// a = a + 3;
+		// b = b + 3;
+		// c = c + 3;
+		// }
+
+		// for (int a = 0; a < 100; a = a + 10) {
+		// for (int i = 1; i < 11; i++) {
+		// int b = a + i;
+		// System.out.print(b + " ");
+		// }
+		//
+		// System.out.println("");
+		//
+		// }
+
+		for (int i = 0; i < 6; i++) {
+			System.out.println("*");
 		}
 	}
-	
-	
 }
-
-
