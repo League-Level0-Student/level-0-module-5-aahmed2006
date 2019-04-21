5cb565dadb1f912669412e6ee1ab5181647055cb
aahmed2006 <league@WTS14.attlocal.net>
2019-04-20 17:25:28 -0700

    Code For Life, Skill Practice Worksheet

---
+++ src/SkillPracticeWorksheet.java
@@ -0,0 +1,77 @@
+import javax.swing.JOptionPane;
+
+public class SkillPracticeWorksheet {
+	public static void main(String[] args) {
+
+		SkillPracticeWorksheet skills = new SkillPracticeWorksheet();
+		skills.skill1();
+		skills.skill2();
+		skills.skill3();
+		skills.skill4();
+		skills.skill5();
+	}
+
+	void skill1() {
+		// Use pop-ups for the following.
+		// Ask the user how many dimes they have
+		String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+
+		// Tell them how many cents they have (hint multiply by 10)
+		int d = Integer.parseInt(dimes);
+		int cents = d * 10;
+		JOptionPane.showMessageDialog(null, "You have " + cents + " cents");
+
+		// Ask the user how tall they are (inches)
+		String height = JOptionPane.showInputDialog("How tall are you (inches)?");
+		// If they are shorter than 36 inches, tell them to eat their Wheaties
+		int h = Integer.parseInt(height);
+		if (h < 36) {
+			JOptionPane.showMessageDialog(null, "Go eat your Wheaties.");
+		}
+	}
+
+	void skill2() { // Write a loop to print every third number between 1 and 30 to the console
+		for (int i = 0; i < 30; i++) {
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	void skill3() { // Get a random number that is less than 20 and print it to the console
+
+		// Get another random number that is less than 10 and print it to the console
+
+		// Using a pop-up, tell the user the difference between the numbers // Hint: use
+		// subtraction
+
+	}
+
+	void skill4() { // In a pop-up, ask the user for the city they live in
+		String city = JOptionPane.showInputDialog("What city do you live in?");
+		// If they answered "San Diego", tell them they live in America's Finest City
+		if (city == "San Diego") {
+			JOptionPane.showMessageDialog(null, "You live in America's Finest City!");
+		}
+		// Otherwise, tell them to move to San Diego
+		else {
+			JOptionPane.showMessageDialog(null, "Move to San Diego.");
+		}
+		// Create a variable - cars - and initialize it to the number of cars your
+		// family has. // If there are 0 cars, use a pop-up to display, "I bet you use
+		// public transportation."
+		int cars = 1;
+		// If there is 1 car, use a pop-up to display the make/model of the car
+		JOptionPane.showMessageDialog(null, "The make/model of the car is a Honda CRV");
+		// If there is more than 1 car, use a pop-up to display how many wheels the //
+		// cars have between them.
+
+	}
+
+	void skill5() { // In a pop-up, ask the user for the name of their school
+		String school = JOptionPane.showInputDialog("What is the name of your school?");
+		// In another pop-up, tell the user, that their school is a fantastic school. //
+		// You must include the name of the school in the message.
+		JOptionPane.showMessageDialog(null, "Your school, " + school + " is a fantastic school!");
+	}
+}
--- src/extra/Code4Life.java
+++ src/extra/Code4Life.java
@@ -6,23 +6,32 @@ package extra;
  */
 
 import java.applet.AudioClip;
-import java.io.File;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
+	public static void main(String[] args) {
 
-	// 1. Ask the user how many hours they spent coding this week.
+		// 1. Ask the user how many hours they spent coding this week.
+		String hours = JOptionPane.showInputDialog("How many hours haev you spent coding this week?");
+		// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
+		// write code instead.
+		int h = Integer.parseInt(hours);
+		if (h < 3) {
+			JOptionPane.showMessageDialog(null, "Stop watching YouTube and write some code instead.");
+		}
+		// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
+		// they're a Code Ninja
+		else if (h > 2 && h < 6) {
+			JOptionPane.showMessageDialog(null, "You are a Code Ninja.");
+		}
 
-	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
-	// write code instead.
-
-	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
-	// they're a Code Ninja
-
-	// 4. If it is more than 5, call the method below to play the Batman theme song.
+		// 4. If it is more than 5, call the method below to play the Batman theme song.
+		else if (h > 5) {
+			playBatmanTheme();
+		}
+	}
 
 	private static void playBatmanTheme() {
 		try {
@@ -33,5 +42,4 @@ public class Code4Life {
 			ex.printStackTrace();
 		}
 	}
-
 }
